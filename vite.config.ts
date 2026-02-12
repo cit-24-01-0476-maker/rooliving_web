83672808d503cecb843cd0262b65790d81619911
S.A.Kavindu Oshadha Perera <cit-24-01-0476@sltc.ac.lk>
2026-02-12 14:44:04 +0530

    Change base path in Vite config to root
    Updated the base path in the Vite configuration from '/rooliving_web/' to '/' for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/rooliving_web/",
+  // මෙතන තිබුන "/rooliving_web/" අයින් කරලා "/" දැම්මා
+  base: "/", 
 
   plugins: [react()],
   resolve: {
